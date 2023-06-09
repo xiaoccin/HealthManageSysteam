f89d32748a7404e4a56e0c851a882624b57fda41
xiaocc <1062353900@qq.com>
2023-06-09 11:44:03 +0800

    first

---
+++ SchoolManageSystem/src/main/java/com/xiaocc/schoolmanagesystem/SchoolManageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.xiaocc.schoolmanagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManageSystemApplication.class, args);
+	}
+
+}
